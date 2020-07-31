260a95a8f9d765333e4397d0c26b80296d689fe2
unknown <ugborjohn@gmai.com>
2020-07-31 16:58:43 +0100

    add products inputs debugged and is now working

--- src/components/products_dropdown.js
+++ src/components/products_dropdown.js
@@ -1,66 +1,81 @@
-import React, {useState} from 'react'
 
-function Product(){
-const [inputList, setInputList] = useState([{productName:"",price:"",quantity:""}]);
+import React, { useState } from "react";
 
-const handleAddProductDiv =()=>{
-    
-        setInputList([...inputList, {productName:"",price:"",quantity:""}])
-    
+function Product() {
+  const [inputList, setInputList] = useState([{product:"fuel",price:"",quantity:""}]);
 
-}
-
-const handleRemoveProductDiv =(e,index)=>{
-  
-        const list = [...inputList];
-        list.splice(index,1);
-        setInputList(list);
-        setInputList(list);
-    
-    
-    }
-const handleProductDivChange =(e,index)=>{
-    const {name, value} =e.target;
+  // handle input change
+  const handleInputChange = (e, index) => {
+    const { name, value } = e.target;
     const list = [...inputList];
-    list[index,name] = value;
+    list[index][name] = value;
     setInputList(list);
+  };
 
+  // handle click event of the Remove button
+  const handleRemoveClick = index => {
+    const list = [...inputList];
+    list.splice(index, 1);
+    setInputList(list);
+  };
 
-    
-    }
+  // handle click event of the Add button
+  const handleAddClick = () => {
+    setInputList([...inputList, {product:"fuel",price:"",quantity:""}]);
+  };
 
-  return( 
- inputList.map((forminput, index)=>{
-     return(
-         
-        <React.Fragment>
-        <div className ="form-row form-margin">
-        <div className = "col">
-            <label  >Product</label>
-
-            <select name="productName" className ="form-control" value ={forminput.productName} onChange={e=>handleProductDivChange(e,index)}>
-                <option  name = "fuel">Fuel</option>
-                <option   name = "gas">Gas</option>
-                <option name = "diesel">Diesel</option>
-            </select></div>
-        <div className = "col">
-            <label  >Amount(NGN)</label>
-            <input type ="text" value ={forminput.price} onChange={e=>handleProductDivChange(e,index)} className = "form-control"/>
-        </div>
-        <div className = "col">
-            <label  >Quantity(Ltr/KG)</label>
-            <input type ="text" value ={forminput.quantity} onChange ={e =>handleProductDivChange(e,index)} className = "form-control"/>
-            </div>
-            <div className="col">
-            {inputList.length-1==index &&<input type ="button" value ="+" className ="btn" onClick ={handleAddProductDiv} />}
-            {inputList.length!==1 && <input type ="button" value ="-" className ="btn" onClick ={()=>handleRemoveProductDiv(index)} />}
-            </div>
+  return (
+    <div className="App">
+     <div className ="form-row form-margin">
+        <div className = "col"><label  >Product</label></div>
             
+              
+        <div className = "col"><label  >Amount(NGN)</label></div>
+        <div className = "col"><label  >Quantity</label></div>
+        <div className = "col"><label  ></label></div>
+        </div>
+        
+      {inputList.map((x, i) => {
+        return (
+          <React.Fragment>
+              <div className ="form-row form-margin">
+          <div className="col">
+          <select name="product" className ="form-control"  value ={x.product} 
+       onChange={e => handleInputChange(e, i)}>
+                <option   value = "fuel">Fuel</option>
+                <option   value = "gas">Gas</option>
+                <option   value = "diesel">Diesel</option>
+            </select></div>
+             <div className="col">
+            <input
+             
+             className ="form-control"
+              name="price"
+			  placeholder=""
+              value={x.price}
+              onChange={e => handleInputChange(e, i)}
+            /></div>
+             <div className="col">
+            <input
+              className ="form-control"
+              name="quantity"
+			  placeholder="Enter"
+              value={x.quantity}
+              onChange={e => handleInputChange(e, i)}
+            /></div>
+            <div className="col">
+    {inputList.length!==1 && <input type ="button" value ="-" className ="btn" onClick={() => handleRemoveClick(i)} />}
+    {inputList.length-1==i &&<input type ="button" value ="+" className ="btn" onClick={handleAddClick}/>}
+          </div>
+          
+          </div>
+          </React.Fragment>
+        );
+      })}
+      <div style={{ marginTop: 20 }}>{JSON.stringify(inputList)}</div>
     </div>
-    <div>{JSON.stringify(inputList)}</div>
-    </React.Fragment>
-     )})
- );
-
+  );
 }
+
+
 export default Product;
